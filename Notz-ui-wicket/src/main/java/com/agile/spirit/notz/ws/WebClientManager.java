6a42ba2a258f9de4644c5668c0e992f08b7b9a4a
AgileSpirit <jbuget@agile-spirit.fr>
2012-08-15 18:33:51 +0200

    Enh: Modify WebClientManager

--- Notz-ui-wicket/src/main/java/com/agile/spirit/notz/ws/WebClientManager.java
+++ Notz-ui-wicket/src/main/java/com/agile/spirit/notz/ws/WebClientManager.java
@@ -8,7 +8,8 @@ import com.sun.jersey.api.client.filter.LoggingFilter;
 
 public class WebClientManager {
 
-  public static final String WEB_SERVICE_URL = "http://localhost:8080/Notz-ws";
+//  public static final String WEB_SERVICE_URL = "http://localhost:8080/Notz-ws";
+  public static final String WEB_SERVICE_URL = "http://notzws.elasticbeanstalk.com";
 
   private static final Client webClient = buildWebClient();
 
