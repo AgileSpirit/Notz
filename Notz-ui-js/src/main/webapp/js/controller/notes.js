a9446bf12bd29d552a0c605ee632e43c11b201f6
AgileSpirit <jbuget@agile-spirit.fr>
2012-09-01 14:46:31 +0200

    Enh: improve note list displaying

--- Notz-ui-js/src/main/webapp/js/controller/notes.js
+++ Notz-ui-js/src/main/webapp/js/controller/notes.js
@@ -27,15 +27,47 @@ function renderNotes(data) {
    // JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
    var list = (data == null || data.note == null) ? [] : (data.note instanceof Array ? data.note : [data.note]);
 
-   $('#noteList li').remove();
+   $('#noteList .note').remove();
    $.each(list, function(index, note) {
-     $('#noteList').append('<li><strong>' + note.title + '</strong>' + note.description + '</li>');
+     var title = note.title;
+     var description = note.description;
+     var modificationDate = new Date(note.modificationDate);
+     var modificationDateHtml = modificationDate.format("dd-mm-yyyy hh:MM:ss")
+
+     var noteHtml = '';
+     noteHtml = noteHtml.concat('<div id="note_' + index + '" class="note">').concat("\n");
+     noteHtml = noteHtml.concat('  <div class="noteHeader">').concat("\n");
+     noteHtml = noteHtml.concat('    <div class="title"><strong>' + title + '</strong></div>').concat("\n");
+     noteHtml = noteHtml.concat('    <div class="actions">').concat("\n");
+     noteHtml = noteHtml.concat('      <a href="#" class="editLink" title="Editer" ></a>').concat("\n");
+     noteHtml = noteHtml.concat('      <a href="#" class="deleteLink" title="Supprimer" ></a>').concat("\n");
+     noteHtml = noteHtml.concat('    </div>').concat("\n");
+     noteHtml = noteHtml.concat('  </div>').concat("\n");
+     noteHtml = noteHtml.concat('  <div class="noteBody">').concat("\n");
+     noteHtml = noteHtml.concat('    <div class="noteDescription">').concat("\n");
+     noteHtml = noteHtml.concat('      <span class="description">' + description + '</span>').concat("\n");
+     noteHtml = noteHtml.concat('    </div>').concat("\n");
+     noteHtml = noteHtml.concat('  </div>').concat("\n");
+     noteHtml = noteHtml.concat('  <div class="noteFooter">').concat("\n");
+     noteHtml = noteHtml.concat('    <div class="metadata">').concat("\n");
+     noteHtml = noteHtml.concat('      <span>' + modificationDateHtml + '</span>').concat("\n");
+     noteHtml = noteHtml.concat('    </div>').concat("\n");
+     noteHtml = noteHtml.concat('    <div class="links">').concat("\n");
+     noteHtml = noteHtml.concat('    </div>').concat("\n");
+     noteHtml = noteHtml.concat('  </div>').concat("\n");
+     noteHtml = noteHtml.concat('</div>').concat("\n");
+     
+     $('#noteList').append(noteHtml);
    });
 }
 
 /*
  * NOTE CREATION
  */
+
+$("#saveNewNoteButton").click(function() {
+  $("#noteCreationForm").submit();
+});
 
 $("#noteCreationForm").submit(function(event) {
   event.preventDefault();
